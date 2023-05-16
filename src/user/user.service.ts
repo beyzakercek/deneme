8abd07f75e2bbc4e72718dd8fa7a2ca4a1df156a
beyza.kercek <beyza.kercek@tirport.com>
2023-05-16 09:57:24 +0300

    resource service created and connected to mongodb

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,54 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { UserModel } from '../../tools/models/user.model';
-import { UserCreateDto } from '../../tools/dtos/user.dto';
+import { UserCreateDto, UserUpdateDto } from '../../tools/dtos/user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ResourceService } from '../../libs/services/resource.service';
 
 const result: UserModel[] = [];
 
 @Injectable()
-export class UserService {
-  getAllUsers(): UserModel[] {
-    if (result.length === 0) {
-      this.creatingMockUser({
-        name: 'Beyza',
-        surname: 'Kercek',
-        birthDay: new Date(),
-        email: 'beyzakercek@gmail.com',
-        password: '123123',
-      });
-    }
-    return result;
-  }
-
-  getUserById(id): any {
-    const user = result.find((data) => data.id == id);
-
-    if (!user) {
-      return 'user does not exist';
-    } else {
-      return user;
-    }
-  }
-
-  createUser(body: UserCreateDto) {
-    const isExist = result.find((res) => {
-      res.email === body.email;
-    });
-    if (isExist) {
-      return isExist;
-    } else {
-      this.creatingMockUser(body);
-      return result.slice(result.length - 1, result.length);
-    }
-  }
-
-  private creatingMockUser(data: any) {
-    const user: UserModel = new UserModel();
-    user.name = data.name;
-    user.email = data.email;
-    user.birthDay = data.birthDay;
-    user.surname = data.surname;
-    user.password = data.password;
-    user.id = (Math.floor(Math.random() * 60) + 1).toString();
-    result.push(user);
+export class UserService extends ResourceService<
+  UserModel,
+  UserCreateDto,
+  UserUpdateDto
+> {
+  constructor(@InjectModel('User') userMongo: Model<UserModel>) {
+    super(userMongo);
   }
 }
